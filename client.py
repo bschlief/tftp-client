c26f3bb2532461a4b1e55e9526c780b558caef15
Bryan Schlief <bschlief@gmail.com>
2014-10-30 01:04:10 -0700

    Refactor common send code into a single function.

--- client.py
+++ client.py
@@ -3,12 +3,12 @@ import sys
 import struct
 import logging
 
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
 logger = logging.getLogger("tftp-client")
-logger.addHandler(logging.NullHandler())
-logger.setLevel(logging.DEBUG)
+logger.addHandler(console)
 
 PACKET_TIMEOUT = 1.0
-
 OP_RRQ = 1
 OP_WRQ = 2
 OP_ACK = 3
@@ -23,17 +23,29 @@ class FileReader:
         self.host = host
         self.port = port
         self.mode = 'netascii'
+        self.block_number = 114
 
-    def rrq(self):
-        logger.debug("Making a request to {}:{} for file {}", self.host, self.port, self.filename)
+    def send(self, data):
+        self.sock.sendto(data, (self.host, self.port))
+
+    def send_rrq(self):
+        logger.warning("Making a request to {}:{} for file {}".format(self.host, str(self.port), self.filename))
         fmt = '!H{}sB{}sB'.format(len(self.filename), len(self.mode))
         data = struct.pack(fmt, OP_RRQ, self.filename, 0, self.mode, 0)
-        self.sock.sendto(data, (self.host, self.port))
+        self.send(data)
+
+    def send_ack(self):
+        fmt = '!HH'
+        data = struct.pack(fmt, OP_ACK, self.block_number)
+        self.send(data)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
     filename = sys.argv[1]
 
     reader = FileReader(filename, HOST, PORT)
-    reader.rrq()
+    reader.send_rrq()
+    reader.send_ack()
+
 

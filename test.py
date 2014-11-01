4cae49c3d1e3ef873e8b0d97278ba29907279e17
Bryan Schlief <bschlief@gmail.com>
2014-11-01 10:30:31 -0700

    Add test file and add mock library to requirements.

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import mock
+
+from client import FileReader
+
+class TestFileReader(unittest.TestCase):
+
+    def test_a_thing(self):
+        self.assertTrue(False)
+
+if __name__ == "__main__":
+    print unittest.main()
